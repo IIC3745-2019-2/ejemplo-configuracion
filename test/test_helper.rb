5f746512526b19730febf9e11e1d0b104a2dca3e
Rodrigo Saffie <rsaffie@gmail.com>
2019-10-10 00:10:45 -0300

    Configure simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/channels/'
+  add_filter '/jobs/'
+  add_filter '/mailers/'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
